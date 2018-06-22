73976ebdd52aad8bb0e724b36b2fb4f932f8a622
loekas <loekasph@gmail.com>
2018-06-22 09:47:45 +0200

    added project 4

--- main.m
+++ main.m
@@ -9,7 +9,9 @@ run('../vl_feat Toolbox/toolbox/vl_setup.m');
 
 %% load files
 im1 = im2single(rgb2gray(imread('8ADT8604.jpg')));
-[f1,d1] = vl_sift(im1,'Octaves', 1, 'Levels', 3, 'FirstOctave', 0, 'EdgeThresh',2);
+im2 = im2single(rgb2gray(imread('8ADT8603.jpg')));
+[f1,d1] = vl_sift(im1);
+[f2,d2] = vl_sift(im2);
 
 figure(1)
 subplot(1,2,1)
@@ -19,4 +21,19 @@ vl_plotsiftdescriptor(d1, f1) ;
 title('Left image with keypoints and discriptors','FontSize',16)
 
 % Show a discriptor in image 1
+subplot(1,2,2)
 title('Left image with keypoints and a discriptor','FontSize',16)
+imagesc(im2); colormap gray; hold on ;
+vl_plotframe(f2);
+vl_plotsiftdescriptor(d2, f2) ;
+
+[matches, scores] = vl_ubcmatch(d1,d2);
+
+indexes = sort(scores,'ascend');
+
+matches_sort = matches(:, indexes);
+feat1_sort = f1(:,matches_sort(1,:));
+feat2_sort = f2(:,matches_sort(2,:));
+
+figure;
+plotmatches(im1,im2,feat1_sort,feat2_sort,matches_sort);
